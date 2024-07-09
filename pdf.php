050e1803079f12ae13d738d95352db29e293b93a
m0b <git@m0b.pp.ua>
2024-07-09 15:31:05 +0000

    Upd & refactor.

--- pdf.php
+++ pdf.php
@@ -4,11 +4,14 @@ require 'vendor/autoload.php';
 
 use Dompdf\Dompdf;
 
-$file = __DIR__ . '/cv-masnukha.html';
+$name = 'cv-masnukha';
+$file = __DIR__ . DIRECTORY_SEPARATOR . $name . '.html';
 $cv = file_get_contents($file);
+
+$cv = str_replace('<body id="browser">', '<body id="pdf">', $cv);
 
 $pdf = new Dompdf();
 $pdf->setPaper('A4');
 $pdf->loadHtml($cv);
 $pdf->render();
-$pdf->stream();
+$pdf->stream($name);
---
+++ web/index.php
@@ -0,0 +1,3 @@
+<?php
+
+include '../cv-masnukha.html';
